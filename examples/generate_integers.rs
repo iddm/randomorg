ef458b36ced03c0d5f3ce251902364cadf99cd30
Victor Polevoy <fx@thefx.co>
2017-12-19 11:55:53 +0300

    Update dependencies

--- examples/generate_integers.rs
+++ examples/generate_integers.rs
@@ -3,6 +3,6 @@ extern crate randomorg;
 fn main() {
     use randomorg::Random;
 
-    let r = Random::new("API KEY HERE").unwrap();
+    let r = Random::new("API KEY HERE");
     println!("Result: {:?}", r.generate_integers(-100, 100, 15, true));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
 //! A random.org api bindings.
-//! 
+//!
 //! The randomness comes from atmospheric noise, which for many purposes is better than the
 //! pseudo-random number algorithms typically used in computer programs. To use the library you
 //! must have an api key which you may get [from here](https://api.random.org/api-keys/beta).
-//! 
+//!
 //! # Usage
-//! 
+//!
 //! ```rust,no_run
 //! extern crate randomorg;
-//! 
+//!
 //! fn main() {
 //!     use randomorg::Random;
-//!     let r = Random::new("API KEY HERE").unwrap();
+//!     let r = Random::new("API KEY HERE");
 //!     println!("Result: {:?}", r.generate_integers(-100, 100, 15, true));
 //!     let random_data = r.request_integers().min(0).max(100).limit(5).collect::<Vec<i32>>();
 //!     println!("Random integers: {:?}", random_data);
 //! }
 //! ```
-//! 
+//!
 //! # Warning
-//! 
+//!
 //! However the library provides a thread-safe object, the **random.org** service does not allow
 //! to use their API from multithreaded-apps. The service processes incoming calls according the
 //! `request_id` parameter, it must be different for concurrent calls, while this library always
-//! provides `request_id` equal to `1`. So when you use the library from multiple threads the 
-//! library will send multiple requests simultaneously with the same `request_id` field and the 
+//! provides `request_id` equal to `1`. So when you use the library from multiple threads the
+//! library will send multiple requests simultaneously with the same `request_id` field and the
 //! **random.org** service will refuse to process them.
 
 #[macro_use]
@@ -83,14 +83,15 @@ pub use results:: {
     GenerateUUIDsResult,
     GenerateBlobsResult,
 };
+
 pub use error::{ Error, ErrorCode, ResponseError, Result };
 
 
 const API_INVOKE: &'static str = "https://api.random.org/json-rpc/2/invoke";
 
-fn retry<F: Fn() -> reqwest::RequestBuilder>(f: F)
-    -> Result<reqwest::Response> {
-    let f2 = || check_status(f().send());
+fn retry(mut builder: reqwest::RequestBuilder) -> Result<reqwest::Response> {
+    let builder = &mut builder;
+    let mut f2 = || check_status(builder.send());
     // retry on a ConnectionAborted, which occurs if it's been a while since the last request
     match f2() {
         Err(_) => f2(),
@@ -106,16 +107,26 @@ fn check_status(response: reqwest::Result<reqwest::Response>) -> Result<reqwest:
     Ok(response)
 }
 
-/// Macro only for internal use with the `Random` object (relies on it's fields)
-macro_rules! request {
+/// Macro only for internal use with the `Random` object (relies on it's fields).
+/// Performs a request.
+macro_rules! make_request {
     ($api:ident, $body:expr) => {
         {
-            retry(|| $api.client.post(API_INVOKE)
-                                .body($body)
-                                .header(ContentType::json()))
+            let mut request_builder = $api.client.post(API_INVOKE);
+            request_builder.body($body).header(ContentType::json());
+            retry(request_builder)
         }
     };
 }
+
+/// Macro only for internal use with the `Random` object (relies on it's fields)
+/// Parses a service data from the request's response.
+macro_rules! request {
+    ($api:ident, $request:ident) => {
+        Ok(make_request!($api, serde_json::to_string(&$request)?)?.json()?)
+    };
+}
+
 
 
 /// A random.org api client.
@@ -126,34 +137,34 @@ pub struct Random {
 
 impl Random {
     /// Creates new random.org client.
-    /// 
-    /// # Usage
-    /// 
-    /// ```rust,no_run
-    /// extern crate randomorg;
-    /// 
-    /// fn main() {
-    ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
-    /// }
-    /// ```
-    pub fn new<S: Into<String>>(api_key: S) -> Result<Random> {
-        Ok(Random {
-            client: reqwest::Client::new()?,
-            api_key: ApiKey(api_key.into()),
-        })
-    }
-
-    /// Create a request object for generating random integers
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
+    /// }
+    /// ```
+    pub fn new<S: Into<String>>(api_key: S) -> Random {
+        Random {
+            client: reqwest::Client::new(),
+            api_key: ApiKey(api_key.into()),
+        }
+    }
+
+    /// Create a request object for generating random integers
+    ///
+    /// # Usage
+    ///
+    /// ```rust,no_run
+    /// extern crate randomorg;
+    ///
+    /// fn main() {
+    ///     use randomorg::Random;
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_integers().min(0).max(100).limit(5).collect::<Vec<i32>>();
     ///     println!("Random integers: {:?}", random_data);
     /// }
@@ -163,15 +174,15 @@ impl Random {
     }
 
     /// Create a request object for generating random decimal fractions
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_decimal_fractions().limit(5)
     ///                                                    .decimal_places(4)
     ///                                                    .collect::<Vec<f32>>();
@@ -183,15 +194,15 @@ impl Random {
     }
 
     /// Create a request object for generating random gaussians
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_gaussians().limit(5)
     ///                                            .collect::<Vec<f32>>();
     ///     println!("Random gaussians: {:?}", random_data);
@@ -202,15 +213,15 @@ impl Random {
     }
 
     /// Create a request object for generating random strings
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_strings().limit(5)
     ///                                          .collect::<Vec<String>>();
     ///     println!("Random strings: {:?}", random_data);
@@ -221,15 +232,15 @@ impl Random {
     }
 
     /// Create a request object for generating random UUIDs
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_uuids().limit(5)
     ///                                        .collect::<Vec<String>>();
     ///     println!("Random strings: {:?}", random_data);
@@ -240,15 +251,15 @@ impl Random {
     }
 
     /// Create a request object for generating random blobs
-    /// 
+    ///
     /// # Usage
     ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     let random_data = r.request_blobs().limit(5)
     ///                                        .collect::<Vec<String>>();
     ///     println!("Random strings: {:?}", random_data);
@@ -259,21 +270,21 @@ impl Random {
     }
 
     /// This method generates true random integers within a user-defined range.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateIntegers)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_integers(-100, 100, 15, true));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `min` must be within [-1e9; 1e9] range
     /// * `max` must be within [-1e9; 1e9] range
@@ -285,27 +296,26 @@ impl Random {
                                                    max,
                                                    limit,
                                                    replacement);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// This method generates true random decimal fractions from a uniform distribution across
     /// the [0,1] interval with a user-defined number of decimal places.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateDecimalFractions)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_decimal_fractions(10, 8));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `limit` must be within [1; 1e4] range
     /// * `decimal_places` must be within [1; 20] range
@@ -314,28 +324,27 @@ impl Random {
         let request = GenerateDecimalFractionsRequest::new(self.api_key.clone(),
                                                            limit,
                                                            decimal_places);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// This method generates true random numbers from a Gaussian distribution (also known as a
     /// normal distribution). The form uses a Box-Muller Transform to generate the Gaussian
     /// distribution from uniformly distributed numbers.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateGaussians)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_gaussians(2000, 1100, 100, 4));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `limit` must be within [1; 1e4] range
     /// * `mean` must be within [-1e6; 1e6] range
@@ -352,28 +361,27 @@ impl Random {
                                                     mean,
                                                     standard_deviation,
                                                     significant_digits);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// This method generates true random strings.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateStrings)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::{ Random, AllowedCharacters };
     ///     use std::collections::BTreeSet;
     ///     let chars = "abcde".chars().collect::<BTreeSet<char>>();
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_strings(5, 10, AllowedCharacters(chars)));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `limit` must be within [1; 1e4] range
     /// * `length` must be within [1; 20] range
@@ -384,79 +392,75 @@ impl Random {
                                                   limit,
                                                   length,
                                                   characters);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// This method generates version 4 true random Universally Unique IDentifiers (UUIDs) in
     /// accordance with section 4.4 of RFC 4122.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateUUIDs)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_uuids(5));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `limit` must be within [1; 1e3] range
     pub fn generate_uuids(&self, limit: u16) -> Result<Response<GenerateUUIDsResult>> {
         let request = GenerateUUIDsRequest::new(self.api_key.clone(), limit);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// This method generates Binary Large OBjects (BLOBs) containing true random data.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#generateBlobs)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.generate_blobs(5, 16));
     /// }
     /// ```
-    /// 
+    ///
     /// # Constraints
     /// * `limit` must be within [1; 100] range
     /// * `size` must be within [1, 1048576] range
     pub fn generate_blobs(&self, limit: u16, size: u32) -> Result<Response<GenerateBlobsResult>> {
         let request = GenerateBlobsRequest::new(self.api_key.clone(), limit, size);
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 
     /// Returns information related to the usage of a given API key.
-    /// 
+    ///
     /// * [Official documentation](https://api.random.org/json-rpc/2/basic#getUsage)
-    /// 
+    ///
     /// # Usage
-    /// 
+    ///
     /// ```rust,no_run
     /// extern crate randomorg;
-    /// 
+    ///
     /// fn main() {
     ///     use randomorg::Random;
-    ///     let r = Random::new("API KEY HERE").unwrap();
+    ///     let r = Random::new("API KEY HERE");
     ///     println!("Result: {:?}", r.get_usage());
     /// }
     /// ```
     pub fn get_usage(&self) -> Result<Response<GetUsageResult>> {
         let request = EmptyRequest::new(Method::GetUsage, self.api_key.clone());
-        let body = serde_json::to_string(&request)?;
-        Ok(request!(self, body.as_str())?.json()?)
+        request!(self, request)
     }
 }
 
